92271b6c638aebf82278262a17e7b0a42e867348
aoliden001 <ainhoa.oliden@ehu.eus>
2018-06-29 09:12:59 +0200

    How to plot graphics with a loop

---
+++ plots-loop.py
@@ -0,0 +1,35 @@
+
+import numpy
+import matplotlib.pyplot
+#import operating system
+import os
+import matplotlib.backends.backend_pdf
+
+pdf = matplotlib.backends.backend_pdf.PdfPages("plots.pdf")
+
+for filename in os.listdir('C:/Users/Ainhoa/Desktop/Python/data'):
+	if filename.startswith("inflammation"):
+		
+		data = numpy.loadtxt(fname=filename, 
+		delimiter=',')
+
+		fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+
+		axes1 = fig.add_subplot(1, 3, 1)
+		axes2 = fig.add_subplot(1, 3, 2)
+		axes3 = fig.add_subplot(1, 3, 3)
+
+		axes1.set_ylabel('average')
+		axes1.plot(numpy.mean(data, axis=0))
+
+		axes2.set_ylabel('max')
+		axes2.plot(numpy.max(data, axis=0))
+
+		axes3.set_ylabel('min')
+		axes3.plot(numpy.min(data, axis=0))
+
+		fig.tight_layout()
+		#If we wanted to extract the graphs one by one		
+		#matplotlib.pyplot.show()
+		pdf.savefig ( fig)
+pdf.close()
